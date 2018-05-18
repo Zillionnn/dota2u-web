d3ca43e7ce8e11f3f90c280bf4ca956824caa649
Zillion <skyestzhang@163.com>
2018-05-18 21:21:39 +0800

    user vuex

--- src/store/index.js
+++ src/store/index.js
@@ -3,12 +3,15 @@ import Vuex from 'vuex';
 
 import player from './modules/player';
 import matchDetail from './modules/matchDetailModule';
+import users from './modules/usersModule';
+
 import 'es6-promise/auto';
 Vue.use(Vuex);
 
 export default new Vuex.Store({
     modules:{
         player,
-        matchDetail
+        matchDetail,
+        users
     }
 });
