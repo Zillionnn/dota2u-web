d0b21aec70c10751398d672b7f804831e3db7252
Zillion <skyestzhang@163.com>
2018-06-17 00:21:46 +0800

    import vuetify

--- src/main.js
+++ src/main.js
@@ -4,8 +4,13 @@ import Vue from 'vue';
 import App from '@/App';
 import router from '@/router';
 import store from '@/store';
+import 'vuetify/dist/vuetify.min.css';
+import colors from 'vuetify/es5/util/colors'
+import Vuetify from 'vuetify';
 
 Vue.config.productionTip = false;
+Vue.use(Vuetify);
+Vue.use(colors)
 
 new Vue({
   el: '#app',
