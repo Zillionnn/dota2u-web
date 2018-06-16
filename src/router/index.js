1d6ee28daafaecd0411c20e40aafb14a62a2b22a
Zillion <skyestzhang@163.com>
2018-06-16 18:22:10 +0800

    reset files constrctor

--- src/router/index.js
+++ src/router/index.js
@@ -1,16 +1,16 @@
 import Vue from 'vue';
 import Router from 'vue-router';
 
-import Player from '@/components/Player';
-import Parent from '@/components/Parent';
-import MatchDetail from '@/components/MatchDetail';
+import Player from '@/components/views/player/Player';
+import Parent from '@/components/views/Parent';
+import MatchDetail from '@/components/views/player/matchDetail/MatchDetail';
 import Index from '@/components/Index';
-import PlayerAllMatchesComponent from '@/components/PlayerAllMatchesComponent';
-import RecentMatchesComponent from '@/components/RecentMatchesComponent';
+import PlayerAllMatchesComponent from '@/components/views/player/allMatch/playerAllMatchesComponent.vue';
+import RecentMatchesComponent from '@/components/views/player/recentMatch/RecentMatchesComponent';
 import Child from '@/components/child';
-import Signup from '@/components/Signup';
+import Signup from '@/components/views/Signup';
 import BindAccountID from '@/components/BindAccountID';
-import Signin from '@/components/Signin';
+import Signin from '@/components/views/Signin';
 
 Vue.use(Router);
 
@@ -34,7 +34,7 @@ export default new Router({
         {
           // 当 /player/:account_id/allMatches 匹配成功，
           // PlayerAllMatchesComponent 会被渲染在 User 的 <router-view> 中
-          path: 'allMatches',
+          path: '/allMatches',
           name: 'allMatches',
           component: PlayerAllMatchesComponent
         }
